46c3d0b39c5974da1c5180e3be1b49a0696fb82e
Karletty Elías <75053017+Karletty@users.noreply.github.com>
2023-04-13 10:18:07 -0600

    Update config.php

--- Core/config.php
+++ Core/config.php
@@ -1,7 +1,3 @@
 <?php
 const PATH = '/ProyectoCatedraKarle';
 const VERIFY_PATH = 'http://localhost' . PATH . '/Clients/verify';
-const MAIL_CREDENTIALS = [
-      'userName' => "lacuponerakarle@gmail.com",
-      'token' => "hbvykdlvrhcbgmqg"
-];
